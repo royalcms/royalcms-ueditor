3ccbc73cfc1a00c7c0fba8c86b67aa90eaeaa40f
王正东 <hxroyal@gmail.com>
2017-01-11 18:16:38 +0800

    修复Input类更换为RC_Request类

--- Royalcms/Component/UEditor/configs/ueditor.php
+++ Royalcms/Component/UEditor/configs/ueditor.php
@@ -1,9 +1,131 @@
 <?php
+//
+//    ______         ______           __         __         ______
+//   /\  ___\       /\  ___\         /\_\       /\_\       /\  __ \
+//   \/\  __\       \/\ \____        \/\_\      \/\_\      \/\ \_\ \
+//    \/\_____\      \/\_____\     /\_\/\_\      \/\_\      \/\_\ \_\
+//     \/_____/       \/_____/     \/__\/_/       \/_/       \/_/ /_/
+//
+//   上海商创网络科技有限公司
+//
+//  ---------------------------------------------------------------------------------
+//
+//   一、协议的许可和权利
+//
+//    1. 您可以在完全遵守本协议的基础上，将本软件应用于商业用途；
+//    2. 您可以在协议规定的约束和限制范围内修改本产品源代码或界面风格以适应您的要求；
+//    3. 您拥有使用本产品中的全部内容资料、商品信息及其他信息的所有权，并独立承担与其内容相关的
+//       法律义务；
+//    4. 获得商业授权之后，您可以将本软件应用于商业用途，自授权时刻起，在技术支持期限内拥有通过
+//       指定的方式获得指定范围内的技术支持服务；
+//
+//   二、协议的约束和限制
+//
+//    1. 未获商业授权之前，禁止将本软件用于商业用途（包括但不限于企业法人经营的产品、经营性产品
+//       以及以盈利为目的或实现盈利产品）；
+//    2. 未获商业授权之前，禁止在本产品的整体或在任何部分基础上发展任何派生版本、修改版本或第三
+//       方版本用于重新开发；
+//    3. 如果您未能遵守本协议的条款，您的授权将被终止，所被许可的权利将被收回并承担相应法律责任；
+//
+//   三、有限担保和免责声明
+//
+//    1. 本软件及所附带的文件是作为不提供任何明确的或隐含的赔偿或担保的形式提供的；
+//    2. 用户出于自愿而使用本软件，您必须了解使用本软件的风险，在尚未获得商业授权之前，我们不承
+//       诺提供任何形式的技术支持、使用担保，也不承担任何因使用本软件而产生问题的相关责任；
+//    3. 上海商创网络科技有限公司不对使用本产品构建的商城中的内容信息承担责任，但在不侵犯用户隐
+//       私信息的前提下，保留以任何方式获取用户信息及商品信息的权利；
+//
+//   有关本产品最终用户授权协议、商业授权与技术服务的详细内容，均由上海商创网络科技有限公司独家
+//   提供。上海商创网络科技有限公司拥有在不事先通知的情况下，修改授权协议的权力，修改后的协议对
+//   改变之日起的新授权用户生效。电子文本形式的授权协议如同双方书面签署的协议一样，具有完全的和
+//   等同的法律效力。您一旦开始修改、安装或使用本产品，即被视为完全理解并接受本协议的各项条款，
+//   在享有上述条款授予的权力的同时，受到相关的约束和限制。协议许可范围以外的行为，将直接违反本
+//   授权协议并构成侵权，我们有权随时终止授权，责令停止损害，并保留追究相关责任的权力。
+//
+//  ---------------------------------------------------------------------------------
+//
 /**
  * ueditor完整配置项
  * 可以在这里配置整个编辑器的特性
  */
 return array(
+    
+    'toolbars' => array(
+    	
+        /**
+         * 基础的文字编辑和图片上传。使用场景：一般图文文章发布。
+         */
+        'base' => array(
+        	array(
+        	    'fullscreen', 'source', '|', 'fontfamily', 'fontsize', '|', 'bold', 'italic', 'underline', 'strikethrough', '|', 'forecolor', 'backcolor', '|',
+        	    'insertorderedlist', 'insertunorderedlist', '|', 'selectall', 'cleardoc', 'removeformat',
+        	    '|', 'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+        	    'link', 'unlink', '|', 'emotion', 'simpleupload', 'insertimage',
+        	    '|', 'undo', 'redo',
+        	    '|', 'horizontal', 'preview', 'drafts'
+        	)
+        ),
+        
+        /**
+         * 简单的文字编辑和图片上传。使用场景：简单的回复评论框。
+         */
+        'simple' => array(
+        	array(
+        	    'bold', 'italic', 'underline', 'strikethrough', '|', 'forecolor', '|', 'blockquote', '|',
+        	    'insertorderedlist', 'insertunorderedlist', '|',
+        	    'link', 'unlink', '|', 'emotion'
+        	)
+        ),
+        
+        /**
+         * 侧重于代码的编辑和展现。使用场景：关于代码的技术性文章回复和评论；技术性问答。
+         */
+        'code' => array(
+        	array(
+        	   'source', '|', 'insertcode', 'fontsize', '|', 'bold', 'italic', 'underline', 'strikethrough', '|', 'forecolor', 'backcolor', '|', 'removeformat', '|',
+        	   'insertorderedlist', 'insertunorderedlist', '|', 'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+        	   'link', 'unlink', '|', 'emotion',  'simpleupload', 'insertimage', '|', 'undo', 'redo', '|', 'horizontal', 'preview', 'fullscreen', 'drafts'
+        	)
+        ),
+        
+        /**
+         * 常用的的文字编辑，表格处理和图片、视频、附件上传。使用场景：包含表格，附件，图文文章发布，对文章格式要求较高。
+         */
+        'standard' => array(
+        	array(
+        	    'fullscreen','source', '|', 'paragraph', 'fontfamily', 'fontsize', '|',
+        	    'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain',
+        	    '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', '|',
+        	    'rowspacingtop', 'rowspacingbottom', 'lineheight', 'indent', '|',
+        	    'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+        	    'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+        	    'emotion', 'simpleupload', 'insertimage', 'insertvideo', 'attachment', 'pagebreak', '|',
+        	    'horizontal', 'spechars', '|',
+        	    'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'splittocells', 'charts', '|',
+        	    'undo', 'redo', '|',
+        	    'print', 'preview', 'searchreplace', 'drafts'
+        	)
+        ),
+        
+        /**
+         * 高级完整版，高级复杂的文字和表格编辑，以及其他小功能。当“标准模式”不能满足时使用。
+         */
+        'advanced' => array(
+        	array(
+        	   'fullscreen', 'source', '|', 'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+                'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+                'rowspacingtop', 'rowspacingbottom', 'lineheight', 'indent', '|',
+                'directionalityltr', 'directionalityrtl', '|',
+                'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+                'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+                'emotion', 'simpleupload', 'insertimage', 'insertvideo', 'attachment', 'insertcode', 'pagebreak', 'template', 'background', '|',
+                'horizontal', 'date', 'time', 'spechars', '|',
+                'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
+                'undo', 'redo', '|',
+                'print', 'preview', 'searchreplace', 'drafts'
+        	)
+        ),
+    ),
     
     /**
      * 和原 UEditor /ueditor.config.js 配置完全相同
@@ -29,7 +151,7 @@ return array(
         /* 服务器统一请求接口路径 */
         'serverUrl' => '/index.php/_ueditor/server',
         
-        /* 工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义 */
+        /* 工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义 
         'toolbars' => array(
             array(
                 'fullscreen', 'source', '|', 'undo', 'redo', '|',
@@ -44,7 +166,7 @@ return array(
                 'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
                 'print', 'preview', 'searchreplace', 'drafts', 'help'
             ),
-        ),
+        ),*/
         
         /* 语言配置项,默认是zh_CN。有需要的话也可以使用如下这样的方式来自动多语言切换，当然，前提条件是lang文件夹下存在对应的语言文件： */
         /* lang值也可以通过自动获取 (navigator.language||navigator.browserLanguage ||navigator.userLanguage).toLowerCase() */
